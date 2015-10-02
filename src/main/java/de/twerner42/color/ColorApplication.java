0dc6eb10f36ad0dfc81b327f361435111844dd5f
Torsten Werner <torsten.werner@westernacher.com>
2015-10-02 12:04:46 +0100

    simple summarizing

--- src/main/java/de/twerner42/color/ColorApplication.java
+++ src/main/java/de/twerner42/color/ColorApplication.java
@@ -1,11 +1,21 @@
 package de.twerner42.color;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColorApplication {
+public class ColorApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ColorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        final AngleSet angleSet = new AngleSet();
+        angleSet.add(0);
+        angleSet.getHoles()
+                .map(angleSet::sum)
+                .forEach(System.out::println);
     }
 }
