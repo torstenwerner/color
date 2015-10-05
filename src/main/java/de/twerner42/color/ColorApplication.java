9256c32ab69bbf38dd70f668416d0e5aa96e813c
Torsten Werner <torsten.werner@westernacher.com>
2015-10-05 18:42:53 +0100

    use calculated hue values

--- src/main/java/de/twerner42/color/ColorApplication.java
+++ src/main/java/de/twerner42/color/ColorApplication.java
@@ -1,25 +1,26 @@
 package de.twerner42.color;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ColorApplication implements CommandLineRunner {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
     public static void main(String[] args) {
         SpringApplication.run(ColorApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        try (final PrintWriter writer = new PrintWriter("/tmp/color.csv")) {
-            AngleSet.generate().forEach(writer::println);
-        }
+        final ObjectMapper jsonMapper = new ObjectMapper();
+        final OutputStream outputStream = new FileOutputStream("/tmp/color.json");
+        final List<Integer> angles = AngleSet.generate().boxed().collect(Collectors.toList());
+        jsonMapper.writeValue(outputStream, angles);
     }
 }
