25ff5df266ef67d0a845089a031198c6c1e5ab8f
Torsten Werner <torsten.werner@westernacher.com>
2015-10-06 19:21:43 +0100

    add text to canvas

--- src/main/resources/static/canvas.js
+++ src/main/resources/static/canvas.js
@@ -5,14 +5,18 @@
         for (var i = 0; i < hues.length; i++) {
             context.beginPath();
             var y = (2 * i + 1) * lineWidth;
-            context.moveTo(0, y);
+            context.moveTo(30, y);
             context.lineTo(499, y);
             context.lineWidth = lineWidth;
             context.strokeStyle = 'hsl(' + hues[i] + ', 100%, 50%)';
             context.stroke();
 
+            context.textAlign = 'right';
+            context.textBaseline = 'middle';
+            context.fillText(hues[i], 19, y);
+
             context.beginPath();
-            context.moveTo(0, y + lineWidth);
+            context.moveTo(30, y + lineWidth);
             context.lineTo(499, y + lineWidth);
             context.lineWidth = lineWidth;
             context.strokeStyle = '#ffffff';
@@ -24,7 +28,7 @@
         var context = $element[0].getContext('2d');
         $http.get('angles').
             success(function (data) {
-                $element[0].setAttribute('height', (data.length * lineWidth).toString());
+                $element[0].height = data.length * 2 * lineWidth;
                 draw(context, data);
             });
     });
