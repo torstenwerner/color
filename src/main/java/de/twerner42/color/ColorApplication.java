3c1e44ee52a2f35a7cc8e98e5bcc6234b85bee91
Torsten Werner <torsten.werner@westernacher.com>
2015-10-06 10:41:00 +0100

    allow running performance test from command line

--- src/main/java/de/twerner42/color/ColorApplication.java
+++ src/main/java/de/twerner42/color/ColorApplication.java
@@ -1,11 +1,24 @@
 package de.twerner42.color;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColorApplication {
+public class ColorApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ColorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (args.length != 1) {
+            return;
+        }
+
+        final Integer threadCount = Integer.valueOf(args[0]);
+        final Performance performance = new Performance(threadCount);
+        performance.test();
+        System.exit(0);
     }
 }
