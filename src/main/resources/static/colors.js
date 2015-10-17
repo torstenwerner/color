b75b803f456925430b5411c98e07dca75c3c814d
Torsten Werner <torsten.werner@westernacher.com>
2015-10-17 17:33:28 +0200

    support jetbrains javascript debugger

--- src/main/resources/static/colors.js
+++ src/main/resources/static/colors.js
@@ -1,9 +1,14 @@
 (function () {
     var module = angular.module('Colors', []);
 
-    module.service("HueList", ($http) => ({
-        fetch: () => $http.get('angles').then(response => response.data)
-    }));
+    module.service("HueList", function ($http, $location) {
+        var inDebugger = $location.port() == 63342;
+        var url = inDebugger ? "http://localhost:8080/angles" : "angles";
+
+        return {
+            fetch: () => $http.get(url).then(response => response.data)
+        };
+    });
 
     module.controller('Canvas', ($scope, HueList) => {
         $scope.lineWidth = 5;
